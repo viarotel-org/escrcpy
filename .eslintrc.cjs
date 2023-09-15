a46a5e1154826079975e4e1a8b3a7e0955273f4d
viarotel <viarotel@qq.com>
2023-09-15 10:53:23 +0800

    feat: 🚀 添加 Electron 基本支持

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@electron-toolkit',
+    '@vue/eslint-config-prettier'
+  ],
+  rules: {
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+}

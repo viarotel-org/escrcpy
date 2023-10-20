4b259c3771d8923dec55845b6070d06a36e83e3b
viarotel <viarotel@qq.com>
2023-10-21 01:06:59 +0800

    fix: 🚀 修复 macOS 简单关闭应用重新打开时报错的问题

--- electron/main.js
+++ electron/main.js
@@ -78,14 +78,9 @@ function createWindow() {
   events(mainWindow)
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    mainWindow = null
-  }
+  app.quit()
+  mainWindow = null
 })
 
 app.on('activate', () => {

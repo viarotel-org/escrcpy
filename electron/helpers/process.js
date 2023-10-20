23525930a44c0e9bdd338fb55e11059ad7391484
viarotel <viarotel@qq.com>
2023-10-21 00:21:18 +0800

    fix: 🔧 修复 macOS 无法正确获取系统依赖路径的问题

--- electron/helpers/process.js
+++ electron/helpers/process.js
@@ -1,7 +1,12 @@
 /** 在主进程中获取关键信息存储到环境变量中，从而在预加载脚本中及渲染进程中使用 */
 /** 注意： app.isPackaged 可能被被某些方法改变所以请将该文件放到 main.js 必须位于非依赖项的顶部 */
+import fixPath from 'fix-path'
 
 import { app } from 'electron'
+
+if (process.platform === 'darwin') {
+  fixPath()
+}
 
 process.env.IS_PACKAGED = JSON.stringify(app.isPackaged)
 

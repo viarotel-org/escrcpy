dcfc76ebdaf885c073a7528b5a1ed6e8bfdb0a39
抠脚本人 <742374184@qq.com>
2024-03-11 21:32:37 +0800

    修改设备重命名展示方式为click，避免为hover时输入中文焦点消失

--- src/components/Device/components/Remark/index.vue
+++ src/components/Device/components/Remark/index.vue
@@ -1,9 +1,17 @@
+<!--
+ * @Author: 抠脚本人
+ * @QQ: 742374184
+ * @Date: 2024-03-11 19:53:05
+ * @LastEditTime: 2024-03-11 21:18:46
+ * @Description: 
+ * 灵感来之不易,积累创造奇迹
+-->
 <template>
   <el-popover
     placement="bottom-start"
     :width="250"
-    trigger="hover"
-    @show="handleFocus"
+    trigger="click"
+    @show="handleClick"
   >
     <template #reference>
       <el-tag effect="light" class="cursor-pointer">
@@ -44,7 +52,7 @@ export default {
       // console.log('onChange', value)
       this.$store.device.setRemark(this.device.id, value)
     },
-    async handleFocus() {
+    async handleClick() {
       this.$refs.elInput.focus()
     },
   },

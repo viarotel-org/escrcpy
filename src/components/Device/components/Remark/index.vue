a5cbf194cc870ded3c32ced32641bec6fae798f4
viarotel <viarotel@qq.com>
2024-03-13 10:40:42 +0800

    style: 📝 Update remark code

--- src/components/Device/components/Remark/index.vue
+++ src/components/Device/components/Remark/index.vue
@@ -1,17 +1,9 @@
-<!--
- * @Author: 抠脚本人
- * @QQ: 742374184
- * @Date: 2024-03-11 19:53:05
- * @LastEditTime: 2024-03-11 21:18:46
- * @Description: 
- * 灵感来之不易,积累创造奇迹
--->
 <template>
   <el-popover
     placement="bottom-start"
     :width="250"
     trigger="click"
-    @show="handleClick"
+    @show="onShow"
   >
     <template #reference>
       <el-tag effect="light" class="cursor-pointer">
@@ -19,7 +11,7 @@
           <el-icon>
             <EditPen />
           </el-icon>
-          <span class="pl-1">{{ device.$remark || $t("device.remark") }}</span>
+          <span class="pl-1">{{ device.$remark || $t('device.remark') }}</span>
         </div>
       </el-tag>
     </template>
@@ -30,7 +22,7 @@
       class=""
       :placeholder="$t('common.input.placeholder')"
       clearable
-      @change="onChange"
+      @change="onInputChange"
     ></el-input>
   </el-popover>
 </template>
@@ -46,14 +38,13 @@ export default {
   data() {
     return {}
   },
-  created() {},
   methods: {
-    onChange(value) {
-      // console.log('onChange', value)
-      this.$store.device.setRemark(this.device.id, value)
-    },
-    async handleClick() {
+    async onShow() {
       this.$refs.elInput.focus()
+    },
+
+    onInputChange(value) {
+      this.$store.device.setRemark(this.device.id, value)
     },
   },
 }

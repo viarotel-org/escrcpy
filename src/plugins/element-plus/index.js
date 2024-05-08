603e4e98d251a14ffdde2b48f1f064522154e6fe
viarotel <viarotel@qq.com>
2024-05-08 11:22:09 +0800

    fix: 🐛 Tips unavailable

--- src/plugins/element-plus/index.js
+++ src/plugins/element-plus/index.js
@@ -1,12 +1,21 @@
+import * as ElementPlusIcons from '@element-plus/icons-vue'
+
+import { ElLoading, ElMessage, ElMessageBox } from 'element-plus'
+import 'element-plus/theme-chalk/el-loading.css'
+import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/el-message-box.css'
+
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './restyle.css'
-
-import * as ElementPlusIcons from '@element-plus/icons-vue'
 
 export default {
   install(app) {
     for (const [key, component] of Object.entries(ElementPlusIcons)) {
       app.component(key, component)
     }
+
+    app.use(ElMessage)
+    app.use(ElMessageBox)
+    app.use(ElLoading)
   },
 }

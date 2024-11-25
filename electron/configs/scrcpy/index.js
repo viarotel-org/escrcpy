bf06382b3a66a7efbf01975f1eac0b092e78d034
viarotel <viarotel@qq.com>
2024-11-25 11:33:20 +0800

    perf: 🚀 Update to scrcpy v3.0

--- electron/configs/scrcpy/index.js
+++ electron/configs/scrcpy/index.js
@@ -5,10 +5,10 @@ export const getScrcpyPath = () => {
   switch (process.platform) {
     case 'win32':
       return extraResolve('win/scrcpy/scrcpy.exe')
-    // case 'darwin':
-    //   return extraResolve(`mac-${process.arch}/scrcpy/scrcpy`)
-    // case 'linux':
-    //   return extraResolve('linux/scrcpy/scrcpy')
+    case 'darwin':
+      return extraResolve(`mac/scrcpy/scrcpy`)
+    case 'linux':
+      return extraResolve('linux/scrcpy/scrcpy')
     default:
       return which.sync('scrcpy', { nothrow: true })
   }

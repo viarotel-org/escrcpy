0d606550d437aaf81094a0783ff4986a8135d74a
viarotel <viarotel@qq.com>
2024-11-26 18:13:31 +0800

    fix: 🐛 Fix for built-in scrcpy not supporting x64 macos

--- electron/configs/scrcpy/index.js
+++ electron/configs/scrcpy/index.js
@@ -2,16 +2,19 @@ import { extraResolve } from '$electron/helpers/index.js'
 import which from 'which'
 
 export const getScrcpyPath = () => {
-  switch (process.platform) {
-    case 'win32':
-      return extraResolve('win/scrcpy/scrcpy.exe')
-    case 'darwin':
-      return extraResolve(`mac/scrcpy/scrcpy`)
-    case 'linux':
-      return extraResolve('linux/scrcpy/scrcpy')
-    default:
-      return which.sync('scrcpy', { nothrow: true })
+  if (['win32'].includes(process.platform)) {
+    return extraResolve('win/scrcpy/scrcpy.exe')
   }
+  else if (['darwin'].includes(process.platform)) {
+    if (['arm64'].includes(process.arch)) {
+      return extraResolve(`mac/scrcpy/scrcpy`)
+    }
+  }
+  else if (['linux'].includes(process.platform)) {
+    return extraResolve('linux/scrcpy/scrcpy')
+  }
+
+  return which.sync('scrcpy', { nothrow: true })
 }
 
 export const scrcpyPath = getScrcpyPath()

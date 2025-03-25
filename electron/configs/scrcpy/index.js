5b033fd2a1a6c9cf397b2a9178f3cde03df83266
viarotel <viarotel@qq.com>
2025-03-25 14:56:08 +0800

    perf: 👽️ Experimentally add linux arm64 support

--- electron/configs/scrcpy/index.js
+++ electron/configs/scrcpy/index.js
@@ -2,15 +2,24 @@ import { extraResolve } from '$electron/helpers/index.js'
 import which from 'which'
 
 export function getScrcpyPath() {
+  const whichPath = which.sync('scrcpy', { nothrow: true })
+
   switch (process.platform) {
     case 'win32':
       return extraResolve('win/scrcpy/scrcpy.exe')
+
     case 'darwin':
       return extraResolve(`mac-${process.arch}/scrcpy/scrcpy`)
+
     case 'linux':
-      return extraResolve('linux/scrcpy/scrcpy')
+      if (['arm64'].includes(process.arch)) {
+        return whichPath
+      }
+
+      return extraResolve(`linux-${process.arch}/scrcpy/scrcpy`)
+
     default:
-      return which.sync('scrcpy', { nothrow: true })
+      return whichPath
   }
 }
 

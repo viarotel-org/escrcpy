8dd9bfc046ee51fcd3a1f0448dcd84a63796611b
viarotel <viarotel@qq.com>
2025-04-27 11:35:32 +0800

    docs: 📝 Added Arabic language support

--- src/store/preference/model/common/index.js
+++ src/store/preference/model/common/index.js
@@ -40,20 +40,24 @@ export default {
       placeholder: 'common.language.placeholder',
       options: [
         {
-          label: 'common.language.zh-CN',
+          label: '中文',
           value: 'zh-CN',
         },
         {
-          label: 'common.language.zh-TW',
+          label: '繁體中文',
           value: 'zh-TW',
         },
         {
-          label: 'common.language.en-US',
+          label: 'English',
           value: 'en-US',
         },
         {
-          label: 'common.language.ru-RU',
+          label: 'Русский',
           value: 'ru-RU',
+        },
+        {
+          label: 'عربي',
+          value: 'ar',
         },
       ],
     },

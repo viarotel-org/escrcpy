a7a3a17e022f3d8ed5e8db2b1aadfaa5c7b84a46
viarotel <viarotel@qq.com>
2025-07-19 19:57:13 +0800

    fix: 🐛 Fix typos

--- src/plugins/element-plus/locale.js
+++ src/plugins/element-plus/locale.js
@@ -3,7 +3,7 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import zhTw from 'element-plus/es/locale/lang/zh-tw'
 
 export default {
-  'en-Us': enUs,
+  'en-US': enUs,
   'zh-CN': zhCn,
   'zh-TW': zhTw,
 }

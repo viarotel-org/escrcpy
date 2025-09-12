3fb18bd396651f7baef0297882931f12167e1e45
viarotel <38282758+viarotel@users.noreply.github.com>
2025-09-12 16:14:00 +0000

    Deploying to docs from @ viarotel-org/escrcpy@8b6aa6e726997132d34a9112cbf5c9f97e7c0035 🚀

--- assets/client-DgAkGJeT.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{d()});function d(){try{(window.adsbygoogle=window.adsbygoogle||[]).push({}),console.log("adsbygoogle loaded")}catch(o){console.warn("adsbygoogle error:",o)}}

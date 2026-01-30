27185f596951acfd6767e91dba7ba4bef0ae12f5
viarotel <viarotel@foxmail.com>
2026-01-31 01:13:00 +0800

    refactor: simplify restoreAndFocusWindow options

--- desktop/electron/services/lifecycle/helpers/index.js
+++ desktop/electron/services/lifecycle/helpers/index.js
@@ -6,13 +6,10 @@ export * from './singleton.js'
 
 export function restoreAndFocusWindow(
   win,
-  options = {},
 ) {
   if (!win || win.isDestroyed()) {
     return false
   }
-
-  const { forceFocus = true, showDock = process.platform === 'darwin' } = options
 
   // Restore if minimized
   if (win.isMinimized()) {
@@ -25,12 +22,10 @@ export function restoreAndFocusWindow(
   }
 
   // Focus if requested
-  if (forceFocus) {
-    win.focus()
-  }
+  win.focus()
 
   // Show dock on macOS if requested
-  if (showDock && process.platform === 'darwin') {
+  if (process.platform === 'darwin') {
     electronApp.dock.show()
   }
 
